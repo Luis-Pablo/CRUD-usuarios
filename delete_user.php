67580b11e9934ae920185a7a38283f63afbbafec
Luis Pablo <pabloarias024@gmail.com>
2023-06-10 22:36:17 -0400

    delete user listo

---
+++ delete_user.php
@@ -0,0 +1,13 @@
+<?php
+
+include("connection.php");
+$con = connection();
+
+$id= $_GET['id'];
+
+$sql = "DELETE FROM user WHERE id= '$id'";
+$query = mysqli_query($con, $sql);
+
+if($query){
+    Header("Location: index.php");
+}
